2b82d9adb031a81156116fcaa10bf9e6a6f99159
Milan Silic <silic.milan@gmail.com>
2023-12-09 11:52:15 +0100

    responsive part 1

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 const doc = document.documentElement
 
-
 // NAV
 const nav = document.querySelector('nav')
 window.onscroll = () => window.scrollY > 100 ? nav.classList.add('scr') : nav.classList.remove('scr')
